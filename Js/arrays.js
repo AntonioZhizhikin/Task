90e4a18d312de6cdc2d3183802404f201d7e4771
Anещт <zhizhikiap@mail.ru>
2023-10-31 11:59:57 +0300

    Решили первую задачу

--- Js/arrays.js
+++ Js/arrays.js
@@ -1,7 +1,11 @@
 function task1()
 {
-    let arr=Array.from(Array(10),()=>{return Math.round (Math.random()*20)});
-    console.log(arr.toString());
-    let sum=arr.filter(index=>index%2==0).reduce((sum,elem)=>sum+Math.sqrt(elem),0);
-    console.log(sum);
+    let arr=[4,16,36,25]
+    let sum=arr.reduce(function(sum,elem)
+    {
+        if (elem%2==0) sum+=Math.sqrt(elem);
+        return sum;
+
+    },0);
+    console.log(summa);
 }
