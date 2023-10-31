0c8083d4e3f75f56a8beb04628878d324fb30ec6
Anещт <zhizhikiap@mail.ru>
2023-10-31 12:06:51 +0300

    Решение задачи 1

--- Js/arrays.js
+++ Js/arrays.js
@@ -1,11 +1,12 @@
 function task1()
 {
-    let arr=[4,16,36,25]
-    let sum=arr.reduce(function(sum,elem)
+    let arr=new Array(4,16,36,25);
+    console.log(arr)
+    let summa=arr.reduce(function(sum,elem)
     {
         if (elem%2==0) sum+=Math.sqrt(elem);
         return sum;
 
     },0);
-    console.log(summa);
+    alert (summa);
 }
